94143c4c2bfcdc8cbbeeb00da468e2a79b686e14
Brendan Long <bren.c.long@gmail.com>
2017-03-03 09:49:48 +0000

    Initial Commit

---
+++ stream2_project.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
+
+app = Flask(__name__)
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'movieDeaths'
+COLLECTION_NAME = 'projects'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app_route('movieDeaths/projects')
+def donor_projects():
+
+    fields = {
+        '_id': False,
+        'Film': True,
+        'Year': True,
+        'Body_Count': True,
+        'MPAA_Rating': True,
+        'Genre': True,
+        'Director': True,
+        'Length_Minutes': True,
+        'IMDB_Rating': True
+    }
+
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        projects = collection.find(projection=fields, limit=10000)
+        return json.dumps(list(projects))
+
+
+if __name__ == '__main__':
+    app.run()
