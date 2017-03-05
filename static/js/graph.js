536535e9e9e04e6ef7152d16fbe296a143f23605
Brendan Long <bren.c.long@gmail.com>
2017-03-05 12:06:38 +0000

    Genres added

--- static/js/graph.js
+++ static/js/graph.js
@@ -20,18 +20,30 @@ function makeGraphs(error, projectsJson) {
         return d["Year"];
     });
 
-    // var deathsDim = ndx.dimension(function (d) {
-    //     return d["Body_Count"];
-    // });
+    var movieDim = ndx.dimension(function (d) {
+        return d["Film"];
+    });
+
+    var genreDim = ndx.dimension(function (d) {
+        return d["Genre"];
+    });
 
 
 
 
     //Calculate metrics
     var numMoviesByYear = yearDim.group();
+
     var totalNumDeathsByYear = yearDim.group().reduceSum(function (d) {
         return d["Body_Count"];
     });
+
+    var numDeathsByMovie = movieDim.group().reduceSum(function (d) {
+        return d["Body_Count"];
+    });
+
+    var genres = genreDim.group();
+
 
     //Define values (to be used in charts)
     var minYear = yearDim.bottom(1)[0]["Year"];
@@ -39,33 +51,59 @@ function makeGraphs(error, projectsJson) {
 
 
     //Charts
-    var yearMovieChart = dc.barChart("#year-movie-chart");
-    var yearDeathChart = dc.barChart("#year-death-chart");
+    var moviesPerYearChart = dc.barChart("#year-movie-chart");
+    var deathsPerYearChart = dc.lineChart("#year-death-chart");
+    var deathsPerMovieChart = dc.rowChart("#deaths-movie-chart");
+    var movieGenres = dc.pieChart("#genre-chart");
 
 
-    yearMovieChart
+    moviesPerYearChart
         .width(1650)
         .height(400)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(yearDim)
         .group(numMoviesByYear)
         .transitionDuration(500)
-        .x(d3.time.scale().domain([minYear, maxYear]))
-        .elasticY(true)
+        .x(d3.scale.ordinal().domain([(minYear), (maxYear)]))
+        .xUnits(dc.units.ordinal)
         .xAxisLabel("Year")
+        .elasticX(true)
+        .elasticY(true)
         .yAxis().ticks(10);
 
-    yearDeathChart
+    deathsPerYearChart
         .width(1650)
         .height(400)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(yearDim)
         .group(totalNumDeathsByYear)
         .transitionDuration(500)
-        .x(d3.time.scale().domain([minYear, maxYear]))
-        .elasticY(true)
+        .x(d3.scale.ordinal().domain([(minYear), (maxYear)]))
+        .xUnits(dc.units.ordinal)
         .xAxisLabel("Year")
+        .elasticX(true)
+        .elasticY(true)
         .yAxis().ticks(10);
+
+    deathsPerMovieChart
+        .width(800)
+        .height(500)
+        .dimension(movieDim)
+        .group(numDeathsByMovie)
+        .transitionDuration(500)
+        .xAxis().ticks(10);
+    deathsPerMovieChart.ordering(function (d) { return -d.value});
+    deathsPerMovieChart.rowsCap([10]);
+    deathsPerMovieChart.othersGrouper(false);
+
+    movieGenres
+        .radius(200)
+        .width(800)
+        .height(500)
+        .transitionDuration(1500)
+        .dimension(genreDim)
+        .group(genres);
+
 
 
 
